8bee0161439729f59f18908c97f16fdd66d68077
Jamie Brawn <jameshenrybrawn@gmail.com>
2024-06-18 10:36:23 +0100

    added loading/error handling to article list

--- src/components/ArticleList.jsx
+++ src/components/ArticleList.jsx
@@ -1,23 +1,35 @@
 import { useEffect, useState } from "react";
 import { getArticles } from "../utils/api";
 import ArticleCard from "./ArticleCard";
-import './ArticleList.css'
+import "./ArticleList.css";
 
 const ArticleList = () => {
   const [articles, setArticles] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [err, setErr] = useState(null);
 
   useEffect(() => {
+    setIsLoading(true);
     getArticles()
       .then((response) => {
         setArticles(response);
+        setIsLoading(false);
       })
       .catch((err) => {
         console.error(err);
+        setErr(err);
+        setIsLoading(false);
       });
   }, []);
 
   return (
     <section className="article-list">
+      {isLoading ? <p>Loading...</p> : null}
+      {err ? (
+        <>
+          <h3>Sorry, something went wrong</h3>
+        </>
+      ) : null}
       {articles.map((article) => {
         return <ArticleCard key={article.article_id} article={article} />;
       })}
